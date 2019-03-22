116d02c2caca05a402741c6c8bca59ef4b64cea1
andy <dreamrrrr@hotmail.com>
2019-03-22 14:46:19 +0800

    first commit

---
+++ practice1.py
@@ -0,0 +1,13 @@
+print("aaa")
+
+a = int(input('整數1 = '))
+b = int(input('整數2 = '))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
+
+# c = input()
+# print('c= ', c)
