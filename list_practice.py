67dc65e8dc98775edb8fb3d04c7b57c32f00560d
andy <dreamrrrr@hotmail.com>
2019-03-22 15:33:20 +0800

    加入 list slice

--- list_practice.py
+++ list_practice.py
@@ -5,3 +5,12 @@ for index in range(0, len(list)):
     print(list[index])
 
 
+# list.append(x): 把變數x塞到list的最後面
+# list.insert(i, x): 把變數x塞到i這個位置上
+# list.pop(): 把list的最後一格丟掉
+# list.pop(i): 把list的第i格丟掉
+# list.remove(x): 會把第一個出現的變數x拿掉
+# list.clear(): 把list內的資料全部清光光
+
+#slice
+print(list[0:2])
